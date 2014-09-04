1ed511a620a7d93531815b56d9a8f5b03b780746
Dustin Barnes <dustin.barnes@gmail.com>
2014-09-03 20:41:40 -0700

    Demo code. Don't pretend that you didn't notice this all in 1 commit...

---
+++ step1/Vagrantfile
@@ -0,0 +1,6 @@
+box      = 'phusion/ubuntu-14.04-amd64'
+version  = 2
+
+Vagrant.configure(version) do |config|
+    config.vm.box = box
+end
---
+++ step4/puppet/manifests/devenv.pp
@@ -0,0 +1,11 @@
+group { 'puppet': ensure => 'present' }
+
+exec { "apt-get update":
+  command => "apt-get -yq update",
+  path    => ["/bin","/sbin","/usr/bin","/usr/sbin"]
+}
+
+package { "openjdk-7-jdk":
+  ensure  => installed,
+  require => Exec["apt-get update"],
+}
